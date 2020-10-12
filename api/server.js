fc8293e6334b0d3251536491d05c6dc79d1fa53a
yvette-luong <63530290+yvette-luong@users.noreply.github.com>
2020-10-12 14:48:04 -0700

    install dependencies and set up for migration -> knex configuration -> server

---
+++ api/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+// const KnexSessionStore = require("connect-session-knex")
+
+const server = express();
+
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+server.use(session(sessionConfiguration));
+
+
+server.get("/", (req, res) => {
+    res.json({ api: "up", session: req.session });
+});
